b404ac1638152e84c909a8181b54652133e15034
Alex <zaychenko96y@outlook.com>
2018-10-14 20:17:34 +0300

    Copied from bratok

--- src/routes/comments.routes.ts
+++ src/routes/comments.routes.ts
@@ -1,14 +1,15 @@
 import { Router, Request, Response } from 'express'
 import * as _ from '../services/validations.handler'
 import Comments from '../models/comments.model'
-import { userInfo } from 'os';
 
 export default Router()
   .post(
     '/', 
     _.validationsHandler({
       text: _.$VARCHAR(400),
-      animeId: _.$VARCHAR(10)
+      animeId: _.$GUID(),
+      replyTo: _.$GUID(),
+      likes: [_.$GUID()]
     }),
     async (req: Request, res: Response, next: Function) => {
       try {
