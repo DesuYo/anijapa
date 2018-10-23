f8a3c632d2899408cf9df478981f17eb4931d3d3
Oleh <opavlov@conceptsoft.io>
2018-10-23 21:51:40 +0300

    fix

--- server/services/auth.handler.ts
+++ server/services/auth.handler.ts
@@ -109,7 +109,7 @@ export default (...permissions: string[]): RequestHandler => {
         next(new JsonWebTokenError('User with this token does not exist'))
 
       const user = doc.toObject()
-      if (!user.permissions.some((el: string) => permissions.includes(el) || el === 'overlord'))
+      if (!user.permissions || !user.permissions.some((el: string) => permissions.includes(el) || el === 'overlord'))
         next(new PermissionError())
 
       req.user = user
