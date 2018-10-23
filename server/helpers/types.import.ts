7aa24959a7144dd0b5f9b45b39c418c1edfc8a02
Oleh <opavlov@conceptsoft.io>
2018-10-23 23:59:48 +0300

    test

--- server/helpers/types.import.ts
+++ server/helpers/types.import.ts
@@ -1,6 +1,6 @@
+import { Schema } from 'mongoose'
 export { Request, Response, RequestHandler } from 'express'
 export { Model as MongooseModel, Document as MongooseDocument, Schema as MongooseSchema } from 'mongoose'
 export { JsonWebTokenError, TokenExpiredError } from 'jsonwebtoken'
 export { ValidationErrorItem, SchemaLike as JoiSchemaLike } from 'joi'
-import { Schema } from 'mongoose'
 export type MongooseDocumentID = Schema.Types.ObjectId
